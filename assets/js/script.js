a10a9ed5931fbde4cc014e905c8b141e3427a647
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-30 15:00:10 +0200

    add array + while

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,19 @@ Consegna:
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
 */
+
+listaSpesa = ['pasta', 'pomodori', 'prosciutto', 'formaggio', 'snack', 'latte']
+console.log(listaSpesa);
+
+//DOM
+const ulEl = document.createElement('ul');
+
+i = 0
+while (i < listaSpesa.length) {
+    
+    const el =  listaSpesa[i]
+    console.log(el);
+    
+    
+    i++
+}
